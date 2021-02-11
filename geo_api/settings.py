c80a6cfe15d69d9d0af3ee05ef76759af8c11aaf
MichalJanusz <janusz.j.michal@gmail.com>
2021-02-11 17:13:37 +0100

    configuration and geoloc model

--- geo_api/settings.py
+++ geo_api/settings.py
@@ -37,6 +37,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'my_api',
+    'rest_framework',
 ]
 
 MIDDLEWARE = [
@@ -119,3 +121,5 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
 STATIC_URL = '/static/'
+
+AUTH_USER_MODEL = 'my_api.User'
--- my_api/models.py
+++ my_api/models.py
@@ -1,3 +1,23 @@
+from django.contrib.auth.models import AbstractUser
 from django.db import models
 
+
 # Create your models here.
+
+class User(AbstractUser):
+    pass
+
+
+class GeolocData(models.Model):
+    ip = models.CharField(max_length=64)
+    ip_type = models.CharField(max_length=4)
+    continent_code = models.CharField(max_length=2)
+    continent_name = models.CharField(max_length=32)
+    country_code = models.CharField(max_length=2)
+    country_name = models.CharField(max_length=64)
+    region_code = models.CharField(max_length=2)
+    region_name = models.CharField(max_length=32)
+    city = models.CharField(max_length=32)
+    zip = models.CharField(max_length=16)
+    latitude = models.FloatField(max_length=20)
+    longitude = models.FloatField(max_length=20)
