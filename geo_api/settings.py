ab4c01a8bd13aa49aeb3ed8aabc3d1bff4200629
MichalJanusz <janusz.j.michal@gmail.com>
2021-02-14 20:55:15 +0100

    config for heroku

--- geo_api/settings.py
+++ geo_api/settings.py
@@ -10,6 +10,7 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/3.1/ref/settings/
 """
 
+import django_on_heroku
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -133,3 +134,5 @@ REST_FRAMEWORK = {
         'rest_framework_simplejwt.authentication.JWTAuthentication',
     ],
 }
+
+django_on_heroku.settings(locals())
